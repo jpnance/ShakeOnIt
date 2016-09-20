858bd301542c179ee61c1f19c9038682d8d67142
Patrick Nance <jpnance@gmail.com>
2016-09-20 11:38:44 -0700

    Exporting some of the routing burden to an actual notion of services.

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,9 @@
-var User = require('../models/User');
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/shakeonit');
+var users = require('../services/users');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.send('Shake on it?');
 	});
 
-	app.get('/users', function(req, res) {
-		User.find({}, function(err, docs) {
-			res.send(docs);
-		});
-	});
+	app.get('/users', users.show);
 };
